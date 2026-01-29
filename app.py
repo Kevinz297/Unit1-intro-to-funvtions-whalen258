f37de64a4b17e9569e253ba5c1304001447201fc
Kevinz297 <Kevinz297@nycstudents.net>
2026-01-29 09:25:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+
+x=5
+
+for i in range(180):
+        print(i)
+        t.forward(x)
+        t.left(120)
+
+turtle.done()
